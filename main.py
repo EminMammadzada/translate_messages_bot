51096a47ec68e0069b1a0e13f4c90112b5d77378
Emin Mammadzada <eminmamedzade37@gmail.com>
2021-05-17 20:48:01 +0400

    add /set_languages and /get_current_languages

--- main.py
+++ main.py
@@ -1,48 +1,77 @@
 import telebot
+from telebot import types
 
 API_TOKEN = "1829309173:AAEwaLKc-7UX57EsjdyWfYx-PDlEAO9lEo0"
 LANGUAGES = ["English", "Russian", "Turkish", "Italian", "French"]
 language_from = ""
 language_to = ""
-
 bot = telebot.TeleBot(API_TOKEN)
 
-def create_markup():
-    markup = telebot.types.InlineKeyboardMarkup()
+
+def create_markup(function_name):
+    markup = types.InlineKeyboardMarkup()
     for language in LANGUAGES:
-        markup.add(telebot.types.InlineKeyboardButton(text=language, callback_data=language))
+        markup.add(types.InlineKeyboardButton(text=language, callback_data=f'{function_name}-{language}'))
     return markup
+
+
+def get_from_callback(query):
+    global language_from
+    bot.answer_callback_query(query.id)
+    language_from = query.data.split('-')[1]
+
+
+def get_to_callback(query):
+    global language_to
+    bot.answer_callback_query(query.id)
+    language_to = query.data.split('-')[1]
+
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, f'Thanks for starting me, {message.from_user.first_name}.'
-                     ' I am ready to help you whenever you need. Type /set_languages to continue')
+    bot.send_message(message.chat.id, f'Thanks for starting me, {message.from_user.first_name}. '
+                                      'I am ready to help you whenever you need. Press /help to see available commands')
 
 
 @bot.message_handler(commands=['help'])
 def help(message):
-    bot.send_message(message.chat.id, 'Available Commands:\n/start\n/help\n/set_languages')
+    bot.send_message(message.chat.id, 'Available Commands:\n/start\n/help\n/set_language_from'
+                                      '\n/set_language_to\n/get_current_languages\n/translate')
 
 
-@bot.message_handler(commands=['set_languages'])
-def set_languages(message):
-    bot.send_message(message.chat.id, "Choose the language to translate from:", reply_markup=create_markup())
+@bot.message_handler(commands=['set_language_from'])
+def set_language_from(message):
+    bot.send_message(message.chat.id, "Choose the language to translate from:", reply_markup=create_markup('from'))
+
+
+@bot.message_handler(commands=['set_language_to'])
+def set_language_to(message):
+    bot.send_message(message.chat.id, "Choose the language to translate to:", reply_markup=create_markup('to'))
+
+
+@bot.message_handler(commands=['get_current_languages'])
+def get_current_languages(message):
+    global language_from
+    global language_to
+    bot.send_message(message.chat.id, f"{language_from} ---> {language_to}")
+
+
+@bot.message_handler(commands=['translate'])
+def translate(message):
+    pass
 
 
 @bot.callback_query_handler(func=lambda call: True)
-def test_callback(call):
-    pass
+def iq_callback(query):
+    global language_from
+    global language_to
+    data = query.data
+    if 'from' in data:
+        get_from_callback(query)
+    elif 'to' in data:
+        get_to_callback(query)
+    else:
+        print(f"error happened : {data}")
 
-# def get_language_from(message):
-#     global language_from
-#     language_from = message.text
-#     bot.send_message(message.chat.id, "Choose the language to translate to:", reply_markup=create_markup())
-#     bot.register_next_step_handler_by_chat_id(message.chat.id, get_language_to)
-#
-#
-# def get_language_to(message):
-#     global language_to
-#     language_to = message.text
-#     bot.send_message(message.chat.id, f"You have chosen: {language_from} to {language_to}")
 
 bot.polling()
